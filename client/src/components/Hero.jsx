fff3ce3e797d3cafa2c91a48cf0909d068bbe061
Xolisa Mabope <225039178@stud.cut.ac.za>
2026-08-27 20:05:54 +0200

    Fix CSS import case sensitivity

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,7 +1,7 @@
 
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import "./Hero.css";
+import "./hero.css";
 
 function Hero() {
 
--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -1,6 +1,6 @@
 
 import { Link } from "react-router-dom";
-import "./Footer.css";
+import "./footer.css";
 
 function Footer() {
 
